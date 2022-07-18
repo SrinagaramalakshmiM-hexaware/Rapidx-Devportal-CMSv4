838141f5f38e532b980a7689cb7e47377c92d05f
JaspreetK1-hexaware <JaspreetK1@hexaware.com>
2022-07-18 12:04:02 +0530

    trying database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3000),
-      database: env('DATABASE_NAME', 'demodb'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'admin'),
+      host: env('DATABASE_HOST', 'rapidx-devportal-cmsv4.mysql.database.azure.com'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapidb'),
+      user: env('DATABASE_USERNAME', 'devportaladmin'),
+      password: env('DATABASE_PASSWORD', 'strapi@v4'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
